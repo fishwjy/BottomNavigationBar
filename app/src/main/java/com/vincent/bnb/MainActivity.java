a69983af1062adb16403dead0924ec9c33accc04
fishwjy <fishwjy@163.com>
2019-06-28 21:58:40 +0800

    Migrate to AndroidX

--- app/src/main/java/com/vincent/bnb/MainActivity.java
+++ app/src/main/java/com/vincent/bnb/MainActivity.java
@@ -2,8 +2,8 @@ package com.vincent.bnb;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.vincent.bottomnavigationbar.BottomItem;
